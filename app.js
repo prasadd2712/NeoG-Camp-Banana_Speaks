1c211d7f5ac6b15d911dd597c5897727a85c0bba
prasadd2712 <prasaddeokar27@gmail.com>
2023-06-20 14:28:50 +0530

    index.html

---
+++ app.js
@@ -0,0 +1,17 @@
+// alert("hello moto");
+// var char=prompt("hello type kr");
+// alert(char);
+const temp1=document.querySelector("#btn-translate");
+
+// temp1.addEventListener("click",function clickEventHandler() {
+//     console.log("click me");
+// })
+
+temp1.addEventListener("click",function(){
+    alert("no beta no");
+})
+// })
+
+
+
+
