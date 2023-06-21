266d79c41c9b8df2a1e2834457df009ed13d0841
prasadd2712 <prasaddeokar27@gmail.com>
2023-06-21 15:37:46 +0530

    wiring a text area

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 // alert("hello moto");
 // var char=prompt("hello type kr");
 // alert(char);
-const temp1=document.querySelector("#btn-translate");
+// const temp1=document.querySelector("#btn-translate");
 
-// temp1.addEventListener("click",function clickEventHandler() {
-//     console.log("click me");
+
+
+// temp1.addEventListener("click",function(){
+//     // alert("no beta no");
+//     console.log("clicked");
 // })
 
-temp1.addEventListener("click",function(){
-    alert("no beta no");
-})
-// })
+var textArea2 = document.querySelector("#textarea1");
+console.log(textArea2);
 
-
+// // Add an event listener to the textarea for the 'input' event
+// textArea.addEventListener('mouseover', function() {
+//   const value = textArea.value;
+//   console.log(value);
+// });
 
 
